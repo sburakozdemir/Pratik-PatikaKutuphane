b052e54f8cdfa5375140d43dcf5a4960e2e7dc59
sburakozdemir <sburakozdemir00@gmail.com>
2024-08-06 17:57:15 +0300

    Add project files.

---
+++ Pratik-PatikaKutuphane/Book.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik_PatikaKutuphane
+{
+    // Class (Sınıf): Book bir sınıftır. Sınıflar, nesnelerin şablonlarıdır ve 
+    // nesnelerin özelliklerini ve davranışlarını tanımlar.
+    public class Book
+    {
+        // Property (Özellik): Aşağıdaki özellikler, bir kitabın karakteristiklerini temsil eder.
+        // Bu özellikler, sınıfın verileridir.
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorSurname { get; set; }
+        public int PageCount { get; set; }
+        public string Publisher { get; set; }
+        public DateTime RegistrationDate { get; private set; }
+
+        // Constructor (Yapıcı): Constructorlar, bir nesne oluşturulduğunda çağrılan özel metotlardır.
+        // Default constructor
+        public Book()
+        {
+            Name = string.Empty;
+            AuthorName = string.Empty;
+            AuthorSurname = string.Empty;
+            PageCount = 0;
+            Publisher = string.Empty;
+            RegistrationDate = DateTime.Now;
+        }
+
+        // Parameterized constructor
+        public Book(string name, string authorName, string authorSurname, int pageCount, string publisher)
+        {
+            Name = name;
+            AuthorName = authorName;
+            AuthorSurname = authorSurname;
+            PageCount = pageCount;
+            Publisher = publisher;
+            RegistrationDate = DateTime.Now;
+        }
+
+        // Method to display book information
+        public void DisplayBookInfo()
+        {
+            Console.WriteLine($"{AuthorName} {AuthorSurname}'in {PageCount} sayfalık, {Publisher} yayınevinden çıkan {Name} kitabı {RegistrationDate} tarihinde kaydedildi.");
+        }
+    }
+}
---
+++ Pratik-PatikaKutuphane/Program.cs
@@ -0,0 +1,20 @@
+﻿using Pratik_PatikaKutuphane;
+
+// New: 'new' anahtar kelimesi, bir sınıftan yeni bir nesne oluşturmak için kullanılır.
+// Burada Book sınıfından yeni bir nesne oluşturuyoruz.
+
+// Default constructor kullanarak kitap oluşturma
+Book book1 = new Book();
+book1.Name = "Adı Aylin";
+book1.AuthorName = "Ayşe";
+book1.AuthorSurname = "Kulin";
+book1.PageCount = 398;
+book1.Publisher = "Remzi Kitabevi";
+book1.DisplayBookInfo();
+
+Console.WriteLine("--------------------");
+
+// Parameterized constructor kullanarak kitap oluşturma
+// Bu, nesne oluşturma sırasında özellikleri atamamızı sağlar.
+Book book2 = new Book("Başka Bir Kitap", "Orhan", "Pamuk", 350, "İletişim Yayınları");
+book2.DisplayBookInfo();
